b8bb5614352650278daa186b7312e6da86ff9a15
Mohamed Slimane <113265001+MedSlimane@users.noreply.github.com>
2025-07-09 23:06:41 +0100

    feat: add ArrowDown icon import to ProcessSection component

--- components/ProcessSection.tsx
+++ components/ProcessSection.tsx
@@ -3,6 +3,7 @@ import { useRef } from "react"
 import { gsap } from "gsap"
 import { useGSAP } from "@gsap/react"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { ArrowDown } from "lucide-react"
 
 gsap.registerPlugin(ScrollTrigger)
 
