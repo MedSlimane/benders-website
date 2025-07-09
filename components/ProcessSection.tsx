64ecdca365cea9443f4568175bce133d423115eb
Mohamed Slimane <113265001+MedSlimane@users.noreply.github.com>
2025-07-09 23:05:30 +0100

    Remove unused imports from ProcessSection component

--- components/ProcessSection.tsx
+++ components/ProcessSection.tsx
@@ -3,7 +3,6 @@ import { useRef } from "react"
 import { gsap } from "gsap"
 import { useGSAP } from "@gsap/react"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { Search, Rocket, TrendingUp, ArrowDown } from "lucide-react"
 
 gsap.registerPlugin(ScrollTrigger)
 
